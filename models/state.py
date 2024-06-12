3c5cc531fbc34ad28bee16124d7659ba5ce0d2d4
vishaylinp <vishaylinparshotam@gmail.com>
2024-06-12 02:36:34 -0700

    improve

--- models/state.py
+++ models/state.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from os import environ
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -15,25 +17,24 @@ class State(BaseModel, Base):
     name (str): 128 char name of state, cant be null
     """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
-    if (environ['HBNB_TYPE_STORAGE'] == 'db'):
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
         cities = relationship("City", cascade='all, delete', backref='state')
     else:
+        name = ""
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """
             getter method for cities
             Return: list of City instance where state_id == State.id
             """
-            from models import storage
-            from models.city import City
-
-            all_cities = storage.all(City)
+            all_cities = models.storage.all("City").values()
             cities_list = []
 
-            for city in all_cities.values():
-                if City.state_id == self.id:
+            for city in all_cities:
+                if city.state_id == self.id:
                     cities_list.append(city)
-
             return (cities_list)
